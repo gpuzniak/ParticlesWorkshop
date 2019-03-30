1af16fc6e3f496c37f3f273aae7b5f5fd2a8df62
Greg <greg@uken.com>
2019-03-30 13:25:49 -0400

    Slider driving emission.

---
+++ Shuriken/Assets/Scripts/SmokeOrWhatever.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmokeOrWhatever : MonoBehaviour
+{
+    public ParticleSystem TargetParticleSystem;
+    public Slider EmissionSlider;
+
+    void Update()
+    {
+        ParticleSystem.EmissionModule emission = TargetParticleSystem.emission;
+        emission.rateOverTime = EmissionSlider.value;
+    }
+}
