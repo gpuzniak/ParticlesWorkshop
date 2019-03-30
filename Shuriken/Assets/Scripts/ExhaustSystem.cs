a52400d59449ddcd4717d65e50ecb02756dc8a49
Greg <greg@uken.com>
2019-03-30 12:36:55 -0400

    Crystals

--- Shuriken/Assets/Scripts/ExhaustSystem.cs
+++ Shuriken/Assets/Scripts/ExhaustSystem.cs
@@ -6,21 +6,41 @@ public class ExhaustSystem : MonoBehaviour
 {
     public ParticleSystem Spray;
     public List<ParticleCollisionEvent> collisionEvents;
-    public ParticleSystem CrystalParticles;
+    public bool SpawnCrystals = true;
+    public ParticleSystem CrystalParticleSystem;
+    public float CrystalSize = 0.5f;
 
-    void Start()
+    private List<ParticleSystem.Particle> CrystalParticles;
+
+    private void Start()
     {
         collisionEvents = new List<ParticleCollisionEvent>();
+        CrystalParticles = new List<ParticleSystem.Particle>();
     }
 
-    void OnParticleCollision(GameObject other)
+    private void Update()
     {
-        ParticleReactor reactingObject = other.GetComponent<ParticleReactor>();
-        if (reactingObject != null) { 
-            int numCollisionEvents = Spray.GetCollisionEvents(other, collisionEvents);
-            if (numCollisionEvents > 0)
-            {
-                reactingObject.OnCollision(numCollisionEvents, collisionEvents[0].intersection, collisionEvents[0].normal);
+        CrystalParticleSystem.SetParticles(CrystalParticles.ToArray(), CrystalParticles.Count);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = Spray.GetCollisionEvents(other, collisionEvents);
+        if (numCollisionEvents > 0)
+        {
+            ParticleReactor reactiveObject = other.GetComponent<ParticleReactor>();
+
+            if (reactiveObject != null) {
+
+                reactiveObject.OnCollision(numCollisionEvents, collisionEvents[0].intersection, collisionEvents[0].normal);
+            }
+            else if(SpawnCrystals == true) {
+                ParticleSystem.Particle newCrystalParticle = new ParticleSystem.Particle();
+                newCrystalParticle.position = collisionEvents[0].intersection;
+                newCrystalParticle.startSize = CrystalSize * Random.Range(0.5f, 1.5f);
+                newCrystalParticle.rotation3D = Quaternion.LookRotation(Random.onUnitSphere).eulerAngles;
+                //newCrystalParticle.startLifetime = 10f;
+                CrystalParticles.Add(newCrystalParticle);
             }
         }
     }
--- Shuriken/Assets/Scripts/ParticleReactor.cs
+++ Shuriken/Assets/Scripts/ParticleReactor.cs
@@ -9,6 +9,7 @@ public class ParticleReactor : MonoBehaviour
     public Material ReactorMaterial;
     public Color EmissiveReactionColor;
     public Light ReactionLight;
+    public float SpawnInset = 0.2f;
 
     private float glossiness = 0.0f;
     private Color emission = new Color(0.1f, 0.2f, 0.3f, 1f);
@@ -18,7 +19,7 @@ public class ParticleReactor : MonoBehaviour
         glossiness = (float)eventCount / 20f;
         if (Random.Range(0.0f, 1.0f) < SpawnChance)
         {
-            Instantiate(PrefabSpawnedOnCollision, intersection, Quaternion.LookRotation(normal));
+            Instantiate(PrefabSpawnedOnCollision, intersection - normal * SpawnInset, Quaternion.LookRotation(normal));
             emission += EmissiveReactionColor;
         }
     }
@@ -28,7 +29,7 @@ public class ParticleReactor : MonoBehaviour
         glossiness -= 0.01f;
         glossiness = Mathf.Clamp(glossiness, 0.42f, 0.98f);
         ReactorMaterial.SetFloat("_Glossiness", glossiness);
-        emission *= 0.98f;
+        emission *= 0.95f;
         ReactorMaterial.SetColor("_EmissionColor", emission);
         ReactionLight.color = emission;
     }
